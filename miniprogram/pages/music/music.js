e11d3e2386cb371adda53d0a7dd2acf9fb8a9dfd
xtx <2513539241@qq.com>
2021-01-26 14:27:54 +0800

    myComponents

--- miniprogram/pages/music/music.js
+++ miniprogram/pages/music/music.js
@@ -5,7 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
+    imgUrls:[
+      {url:'http://p1.music.126.net/j0gp3gBDRRoqIXxAs0v7oA==/109951165664720877.jpg?imageView&quality=89'},
+      {url:'http://p1.music.126.net/zUv2mRobckK7Tdn2bp9iSA==/109951165664840470.jpg?imageView&quality=89'},
+      {url:'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'},
+      {url:'http://p1.music.126.net/WOoIZuva_umxxzYOvWINLA==/109951165664707565.jpg?imageView&quality=89'},
+      {url:'http://p1.music.126.net/pOXTFta-mhTpZOGhBBWvhQ==/109951165664682857.jpg?imageView&quality=89'},
+      {url:'http://p1.music.126.net/UdSM2BmqY_h_t9HAOzb5dQ==/109951165664710664.jpg?imageView&quality=89'},
+      {url:'http://p1.music.126.net/Z90NF2dHuBYrV6x-U9jJJQ==/109951165664719544.jpg?imageView&quality=89'},
+      {url:'http://p1.music.126.net/vAjwukVm-H0LOqzy4FTJXA==/109951165664851877.jpg?imageView&quality=89'},
+      {url:'http://p1.music.126.net/9Ayx-EeCnuLRWKTcIhGB6g==/109951165664742856.jpg?imageView&quality=89'}
+    ],
 
+    
+    listDetail:[
+      {url:'http://p1.music.126.net/qfKmb5grEFZNTGYKIONg1A==/109951165664253358.jpg?param=140y140',title:'[华语速爆新歌] 那英×姚晨首度联手合唱',number:'9.9亿'},
+      {url:'http://p1.music.126.net/1hzImIzOsRzG0iBSvHOO7w==/109951165511341127.jpg?param=140y140',title:'随情节流淌|富有叙事感的柔软音乐片段',number:'10万'},
+      {url:'http://p1.music.126.net/Xvo6PwBcdOA69ipcpV9YYg==/109951165463253777.jpg?param=140y140',title:'我试着把孤独藏进耳机',number:'494万'},
+      {url:'http://p2.music.126.net/G8lutIIOcFVwMgR5O9HcSg==/109951165663415422.jpg?param=140y140',title:'谷阿莫：5分钟听完电影《最普通的恋爱》',number:'1611'},
+      {url:'http://p1.music.126.net/PJylNWy_2-jI7LRgQ2Cm6w==/109951165649129522.jpg?param=140y140',title:'【翻/原】温柔不是我说 而是你觉得.',number:'239万'},
+      {url:'http://p2.music.126.net/upYxTEuAB9dqqvu2hYlNzg==/109951165663956064.jpg?param=140y140',title:'#祸祸外传# 11 花花 一首写给未来孩子的歌',number:'12514'}
+    ]
   },
 
   /**
