fcc7a51950d18999b742cdb12481102a1965c596
xtx <2513539241@qq.com>
2021-01-30 15:55:18 +0800

    playmusic

--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -1,13 +1,16 @@
 let musiclist=[]
 //正在播放的歌曲index
 let playingIndex=0
+const backgroundAudioManager=wx.getBackgroundAudioManager()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    picUrl:''
+    picUrl:'',
+    isPlaying:false,
+
   },
 
   /**
@@ -20,7 +23,17 @@ Page({
     musiclist=wx.getStorageSync('musiclist')
     this._loadMusicDetail(options.musicId)
   },
-
+  togglePlaying(){
+    if(this.data.isPlaying)
+    {
+      backgroundAudioManager.pause()
+    }else{
+      backgroundAudioManager.play()
+    }
+    this.setData({
+      isPlaying:!this.data.isPlaying
+    })
+  },
   _loadMusicDetail(musicId){
     let music=musiclist[playingIndex]
     console.log(music)
@@ -38,7 +51,39 @@ Page({
       }
     }).then((res)=>{
       console.log(res)
+      const url=res.result.data[0].url
+      if(url===null){
+        wx.showToast({
+          title: '没有权限播放',
+        })
+        backgroundAudioManager.pause()
+        this.setData({
+          isPlaying:false
+        })
+        return
+      }
+      backgroundAudioManager.src=url
+      backgroundAudioManager.title=music.name
+      backgroundAudioManager.coverImgUrl=music.al.picUrl
+      backgroundAudioManager.singer=music.ar[0].name
+      this.setData({
+        isPlaying:true
+      })
     })
+  },
+  onPrev(){
+    playingIndex--
+    if(playingIndex<=0){
+      playingIndex=musiclist.length-1
+    }
+    this._loadMusicDetail(musiclist[playingIndex].id)
+  },
+  onNext(){
+    playingIndex++
+    if(playingIndex===musiclist.length){
+      playingIndex=0
+    }
+    this._loadMusicDetail(musiclist[playingIndex].id)
   },
 
 
